d173e2153a6fe777d5547b5d3d023dbab141b1f0
Ytellon Macedo <ytellonmacedo@gmail.com>
2023-07-13 20:21:31 -0300

    feat: assemble the necessary screens to create/update/delete a file

---
+++ web/src/components/File/FileForm/FileForm.js
@@ -0,0 +1,71 @@
+import {
+  Form,
+  FormError,
+  FieldError,
+  Label,
+  TextField,
+  Submit,
+} from '@redwoodjs/forms'
+
+const FileForm = (props) => {
+  const onSubmit = (data) => {
+    props.onSave(data, props?.file?.id)
+  }
+
+  return (
+    <div className="rw-form-wrapper">
+      <Form onSubmit={onSubmit} error={props.error}>
+        <FormError
+          error={props.error}
+          wrapperClassName="rw-form-error-wrapper"
+          titleClassName="rw-form-error-title"
+          listClassName="rw-form-error-list"
+        />
+
+        <Label
+          name="title"
+          className="rw-label"
+          errorClassName="rw-label rw-label-error"
+        >
+          Title
+        </Label>
+
+        <TextField
+          name="title"
+          defaultValue={props.file?.title}
+          className="rw-input"
+          errorClassName="rw-input rw-input-error"
+          validation={{ required: true }}
+        />
+
+        <FieldError name="title" className="rw-field-error" />
+
+        <Label
+          name="url"
+          className="rw-label"
+          errorClassName="rw-label rw-label-error"
+        >
+          Url
+        </Label>
+
+        <TextField
+          name="url"
+          defaultValue={props.file?.url}
+          className="rw-input"
+          errorClassName="rw-input rw-input-error"
+          validation={{ required: true }}
+        />
+
+        <FieldError name="url" className="rw-field-error" />
+
+        <div className="rw-button-group">
+          <Submit disabled={props.loading} className="rw-button rw-button-blue">
+            Save
+          </Submit>
+        </div>
+      </Form>
+    </div>
+  )
+}
+
+export default FileForm
